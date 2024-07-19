9af67016de7cc0e0927b4d24a17106566f772bcd
AI 10기 강민규 <myname@g.skku.edu>
2024-07-19 15:49:16 +0900

    📰 New modules 폴더 module service controller (audio,auth,characters, images,stories,users) , shared 폴더(filters,guards,interceptors,pipes,utils), config 폴더 🎨 Style app.module.ts main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { ModulesModule } from './modules/modules.module';
+import { UsersController } from './modules/users/users.controller';
+import { StoriesModule } from './modules/stories/stories.module';
+import { ImagesModule } from './modules/images/images.module';
+import { AudioModule } from './modules/audio/audio.module';
+import { CharactersModule } from './modules/characters/characters.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [StoriesModule, ImagesModule, AudioModule,CharactersModule,ConfigModule],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
