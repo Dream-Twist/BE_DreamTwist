b74754fad15d4d83951eb405606bec715c3c2d72
[A10] 박수정 <d-aoo@naver.com>
2024-07-22 17:36:56 +0900

    ✨ Feat: DB 설정

--- main.ts
+++ main.ts
@@ -1,26 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
 /**
-File Name : main.ts
-Description : 메인
+File Name : main
+Description : main.ts
 Author : 강민규
 
 History
-Date        Author   Status    Description
-2024.07.19  강민규   Created
-2024.07.19  박수정   Modified  CORS 추가
+Date        Author      Status      Description
+2024.07.19  강민규      Created     
 */
 
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
 }
+
 //CORS 멀티 리포지토리 통신
 async function repoCORS() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(4000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.listen(4000);
 }
+
 bootstrap();
 repoCORS();
