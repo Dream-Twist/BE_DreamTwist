3e75cd284ebd206603c0dc1f100a4b85ccf149ca
youmin29 <babybana0829@naver.com>
2024-08-03 03:02:35 +0900

    ✨ Feat: AI 스토리 생성 시 포인트 사용 내역 추가

--- src/modules/fairytale/ai-fairytale-create.service.ts
+++ src/modules/fairytale/ai-fairytale-create.service.ts
@@ -18,6 +18,7 @@ import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 import { CreateAIFairytaleDto } from 'src/modules/fairytale/dto/ai-fairytale-create.dto';
 import { PointRepository } from 'src/modules/billing/repository/point.repository';
+import { PointHistoryRepository } from 'src/modules/billing/repository/point-history.repository';
 import { AIFairytaleType } from 'src/modules/fairytale/type/ai-fairytale-create.type';
 
 @Injectable()
@@ -29,6 +30,8 @@ export class AIFairytaleService {
         private configService: ConfigService,
         @InjectRepository(PointRepository)
         private readonly pointRepository: PointRepository,
+        @InjectRepository(PointHistoryRepository)
+        private readonly pointHistoryRepository: PointHistoryRepository,
     ) {
         this.aiUrl = this.configService.get('AI_SERVER_URL');
     }
@@ -37,17 +40,29 @@ export class AIFairytaleService {
         const { prompt } = createAIFairytaleDto;
 
         // 임시
-        const user_id = 2;
+        const user_id = 2; // 유저ID
+        const storyPoints = 3; // 스토리 생성 시 사용되는 포인트
 
         // 해당 사용자의 현재 포인트 검색
         const pointsByUser = await this.pointRepository.findPointByUserId(user_id);
 
         // 포인트 확인(일단 ai 스토리 생성이 1포인트라고 가정)
-        if (pointsByUser.points < 1 || !pointsByUser) {
+        if (pointsByUser.points < storyPoints || !pointsByUser) {
             throw new ForbiddenException('포인트가 부족합니다.');
         }
 
-        await this.pointRepository.updatePointByUserId(user_id, -1);
+        // 포인트 관련
+        await Promise.all([
+            // 유저 포인트 변경
+            this.pointRepository.updatePointByUserId(user_id, -1 * storyPoints),
+            // 포인트 내역 추가
+            this.pointHistoryRepository.createPointHistory({
+                user_id,
+                points: -1 * storyPoints,
+                description: 'AI 스토리 생성',
+            }),
+        ]);
+
         const res = await firstValueFrom(this.httpService.post(`${this.aiUrl}/story`, { prompt }));
         return res.data;
     }
