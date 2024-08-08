b3316f55ea23185861fa30a34758310d5ea31789
AI 10기 강민규 <myname@g.skku.edu>
2024-08-08 17:04:01 +0900

    ✨ Feat: fairytale-manage 좋아요 생성 회원 연결 (엑세스 토큰 필요) 🎨 Style : fairytale-like.dto.ts 동화 id만 받아서 생성하기

--- src/modules/fairytale/dto/fairytale-like.dto.ts
+++ src/modules/fairytale/dto/fairytale-like.dto.ts
@@ -8,36 +8,15 @@ Date        Author      Status      Description
 2024.08.03  박수정      Created     
 2024.07.20  박수정      Modified    동화 스토리 좋아요 DTO 생성
 2024.08.07  강민규      Modified    리포지토리 기반 추가 행 생성
+2024.08.08  강민규      Modified    동화 ID만 활용
 */
 
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
-import { IsString, IsNotEmpty, IsBoolean, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class LikeFairytaleDto {
     @ApiProperty()
     @IsNumber()
     @IsNotEmpty()
-    userId: number;
-
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
     fairytaleId: number;
-
-    @ApiProperty()
-    @IsString()
-    title: string;
-
-    @ApiProperty()
-    @IsString()
-    theme: '우화' | '환경' | '사랑' | '모험' | '추리' | '기타';
-
-    @ApiProperty()
-    content: string;
-
-    @ApiProperty()
-    @IsBoolean()
-    @Transform(({ value }) => value === 'true' || value === true)
-    privatedAt: boolean;
 }
